4c1a36aa393549138e005485517c380766ed5313
김봄비 <rlawldud627@naver.com>
2022-01-13 20:46:38 +0900

    test: Add input, useState code React Hook Form 없이 form 작성할 때 예시

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 function ToDoList() {
   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
+  // input 갯수만큼 input과 해당 inputError useState만들어야 함
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
@@ -10,13 +12,20 @@ function ToDoList() {
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(toDo);
+    // to do list를 10자 이상 작성하시오(input의 조건에 해당하는 validation 다 작성해야 함)
+    if (toDo.length < 10)
+    {
+      return setToDoError("To do should be longer");
+    }
+    setToDoError("");
+    console.log("submit");
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
         <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
       </form>
     </div>
   );
